75ba823a64cdb6a0e9d36bdb52a4e8d8bf686e84
jamesnguyennm <jamesnguyennm@gmail.com>
2023-08-26 12:17:22 +1000

    Create menu item model

---
+++ menu-service/src/models/menuItem.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const menuItemSchema = new Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+const MenuItem = mongoose.model("MenuItem", menuItemSchema);
+
+module.exports = MenuItem;
