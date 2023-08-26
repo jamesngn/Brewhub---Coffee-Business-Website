56d60900628e156fce64e4dd0f0f22da62292acd
jamesnguyennm <jamesnguyennm@gmail.com>
2023-08-26 12:16:32 +1000

    Add route handling for creating, getting menu item

---
+++ menu-service/src/controllers/menuController.js
@@ -0,0 +1,26 @@
+const MenuItem = require("../models/menuItem");
+
+async function getMenuItems(req, res) {
+  try {
+    const menuItems = await MenuItem.find();
+    res.json(menuItems);
+  } catch (error) {
+    res.status(500).json({ error: "Error retrieving menu" });
+  }
+}
+
+async function createMenuItem(req, res) {
+  const { name, price } = req.body;
+  try {
+    const newItem = new MenuItem({ name, price });
+    await newItem.save();
+    res.json(newItem);
+  } catch (error) {
+    res.status(400).json({ error: "Error adding item to menu" });
+  }
+}
+
+module.exports = {
+  getMenuItems,
+  createMenuItem,
+};
