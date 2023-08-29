1d344924649ee1d66404247d4799d0a8ab48b6f6
James Nguyen <jamesnguyennm@gmail.com>
2023-08-29 18:39:36 +1000

    gRPC client for order-service

---
+++ order-service/client.js
@@ -0,0 +1,24 @@
+//client.js
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader");
+
+//Load the proto files for different gRPC services
+const orderPackageDef = protoLoader.loadSync("order.proto", {});
+const menuPackageDef = protoLoader.loadSync("../menu-service./menu.proto", {});
+
+// Load the gRPC objects for different services
+const menuGrpcObject = grpc.loadPackageDefinition(menuPackageDef);
+const orderGrpcObject = grpc.loadPackageDefinition(orderPackageDef);
+
+// Create gRPC client instances for different services
+const menuClient = new menuGrpcObject.menuPackage.Menu(
+  "localhost:50051",
+  grpc.credentials.createInsecure()
+);
+
+const orderClient = new orderGrpcObject.orderPackage.Order(
+  "localhost:50052",
+  grpc.credentials.createInsecure()
+);
+
+module.exports = { menuClient, orderClient };
