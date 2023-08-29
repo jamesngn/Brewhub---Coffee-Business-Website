103478889e60a87e640fc707548e3116fe4dce28
James Nguyen <jamesnguyennm@gmail.com>
2023-08-29 18:37:02 +1000

    add id field to menu item

--- menu-service/src/models/menuItem.js
+++ menu-service/src/models/menuItem.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const menuItemSchema = new Schema({
+  id: { type: String, required: true },
   name: { type: String, required: true },
   price: { type: Number, required: true },
 });
