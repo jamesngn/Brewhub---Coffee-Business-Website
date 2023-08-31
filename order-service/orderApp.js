221fe6688fdba4e3629c85e29111ddd58a48ea38
James Nguyen <jamesnguyennm@gmail.com>
2023-08-31 13:34:13 +1000

    add comment

--- order-service/orderApp.js
+++ order-service/orderApp.js
@@ -1,15 +1,17 @@
 const menuService = require("./menuService");
 const orderService = require("./orderService");
 
+// Fetch menu items
 menuService.getMenuItems((menuItems) => {
   console.log("Menu items:", menuItems);
 });
-
+// After fetching menu items, create an order
 const userId = "U1";
 const items = [
   { menuItemId: "M1", quantity: 10 },
   { menuItemId: "M2", quantity: 100 },
 ];
-orderService.placeOrder(userId, items, (orderResponse) => {
+
+orderService.createOrder(userId, items, (orderResponse) => {
   console.log("Order response:", orderResponse);
 });
