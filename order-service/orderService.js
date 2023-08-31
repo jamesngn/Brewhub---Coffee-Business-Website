dd65f995a8ff97b50315bf642a1ba9f744bf95ae
James Nguyen <jamesnguyennm@gmail.com>
2023-08-31 14:45:02 +1000

    update for getOrdersByUserId

--- order-service/orderService.js
+++ order-service/orderService.js
@@ -1,22 +1,42 @@
 // orderService.js
 const { orderClient } = require("./client");
 
-function placeOrder(userId, items, callback) {
-  // Define your gRPC call logic here using the orderClient
+function createOrder(userId, items, callback) {
   const request = {
     userId: userId,
     items: items,
   };
 
-  orderClient.placeOrder(request, (error, response) => {
+  orderClient.createOrder(request, (error, response) => {
     if (error) {
       console.error("Error placing order:", error);
+      // Handle the error and return an error response to the callback
+      callback({ error: "Failed to place order" });
       return;
     }
+    // Return the successful response to the callback
+    callback(response);
+  });
+}
+
+function getOrdersByUserId(userId, callback) {
+  const request = {
+    userId: userId,
+  };
+
+  orderClient.getOrdersByUserId(request, (error, response) => {
+    if (error) {
+      console.error("Error getting orders by UserId:", error);
+      // Handle the error and return an error response to the callback
+      callback({ error: "Failed to get orders by UserId." });
+      return;
+    }
+    // Return the successful response to the callback
     callback(response);
   });
 }
 
 module.exports = {
-  placeOrder,
+  createOrder,
+  getOrdersByUserId,
 };
