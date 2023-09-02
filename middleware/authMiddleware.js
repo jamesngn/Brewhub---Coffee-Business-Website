cb744d279172cd77a649343001ce6f89ba4c6606
James Nguyen <jamesnguyennm@gmail.com>
2023-09-02 17:06:07 +1000

    add authorization

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const config = require("../config/config");
 
-const authenticate = (req, res, next) => {
+const authenticateUser = (req, res, next) => {
   // Get the token from the request header
   const token = req.header("x-auth-token");
 
@@ -9,11 +9,9 @@ const authenticate = (req, res, next) => {
   if (!token) {
     return res.status(401).json({ message: "No token, authorization denied" });
   }
-
   try {
     // Verify the token
     const decoded = jwt.verify(token, config.jwt.secretKey);
-
     // Add the user ID from the token to the request object
     req.user = decoded;
 
@@ -24,4 +22,14 @@ const authenticate = (req, res, next) => {
   }
 };
 
-module.exports = authenticate;
+const authoriseAdmin = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    res
+      .status(403)
+      .json({ message: "Access denied. Admin authorization required." });
+  }
+};
+
+module.exports = { authenticateUser, authoriseAdmin };
