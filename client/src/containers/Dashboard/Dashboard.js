d6b495e04cc8b206b469c08ee97d5b336a260ed3
James Nguyen <jamesnguyennm@gmail.com>
2023-09-04 12:25:35 +1000

    add Dashboard container

---
+++ client/src/containers/Dashboard/Dashboard.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { Typography } from "@mui/material";
+import useSocket from "../../hooks/useSocket";
+
+const Dashboard = ({ userId, userRole }) => {
+  const { socket, response } = useSocket(userId, userRole); // Use the useSocket hook
+
+  const handleSend = (data) => {
+    // Send a message using the existing socket connection
+    if (socket) {
+      socket.emit("message", data);
+    }
+  };
+
+  return (
+    <div>
+      <Typography variant="h4">Dashboard</Typography>
+      <Typography variant="body1">Welcome to the Dashboard!</Typography>
+
+      <div>
+        <strong>Server Message:</strong> {response}
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
