de77af4dcbb02835c5f55ba22ea6ddb45bcb5f90
James Nguyen <jamesnguyennm@gmail.com>
2023-09-04 12:26:14 +1000

    add UseSocket hook for connecting socket io server

---
+++ client/src/hooks/useSocket.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+const useSocket = (userId, userRole) => {
+  const [socket, setSocket] = useState(null);
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const socketInstance = io("http://localhost:8080", {
+      query: { userId, userRole },
+    });
+
+    socketInstance.on("connect", () => {
+      console.log(`Websocket connected with userId: ${userId}`);
+    });
+
+    socketInstance.on("message", handleMessage);
+
+    setSocket(socketInstance);
+
+    // Cleanup the WebSocket connection when the component unmounts
+    return () => {
+      socketInstance.disconnect();
+    };
+  }, [userId, userRole]);
+
+  //Function to handle incoming messages
+  const handleMessage = (data) => {
+    setResponse(data);
+  };
+
+  return { socket, response, handleMessage };
+};
+
+export default useSocket;
