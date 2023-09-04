9d2376c5329808c15a48795315218a11cad57c24
James Nguyen <jamesnguyennm@gmail.com>
2023-09-04 12:24:27 +1000

    add auth function in react js

---
+++ client/src/auth/auth.js
@@ -0,0 +1,26 @@
+// auth.js
+import axios from "axios";
+
+async function isAuthenticated() {
+  const authToken = localStorage.getItem("authToken");
+  if (!authToken) {
+    return false; // No token found
+  }
+  try {
+    const response = await axios.get(`http://localhost:5000/auth/user`, {
+      headers: {
+        "x-auth-token": authToken, // Include the authToken in headers
+      },
+    });
+    return response.data; // Token is valid
+  } catch (error) {
+    return false; // Token is not valid
+  }
+}
+
+function isAuthorized(data) {
+  const { role } = data;
+  return role === "admin";
+}
+
+export { isAuthenticated, isAuthorized };
