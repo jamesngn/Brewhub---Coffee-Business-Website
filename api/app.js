37203edff808fcf31d4da09a875fb42cee83294e
James Nguyen <jamesnguyennm@gmail.com>
2023-09-04 12:22:01 +1000

    rename directory

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
+
+const app = express();
 const cors = require("cors");
 
 app.use(bodyParser.json());
@@ -8,9 +9,11 @@ app.use(cors());
 
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
+const orderRoutes = require("./routes/orderRoutes");
 
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
+app.use("/order", orderRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
