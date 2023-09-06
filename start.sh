f28fe27b7b8f955740aa1b629cb753b8498127c5
James Nguyen <jamesnguyennm@gmail.com>
2023-09-06 22:42:07 +1000

    script to automate docker down, build, up, ps

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define the Docker Compose project name
+COMPOSE_PROJECT_NAME="brewhub_app"
+
+# Check if Docker Compose is installed
+if ! command -v docker-compose &> /dev/null; then
+    echo "Docker Compose is not installed. Please install it."
+    exit 1
+fi
+
+# Stop the Docker Compose services
+docker-compose -p $COMPOSE_PROJECT_NAME down
+
+# Build the Docker Compose services
+docker-compose -p $COMPOSE_PROJECT_NAME build
+
+# Start the Docker Compose services in the background
+docker-compose -p $COMPOSE_PROJECT_NAME up -d
+
+# Display the running services
+docker-compose -p $COMPOSE_PROJECT_NAME ps
