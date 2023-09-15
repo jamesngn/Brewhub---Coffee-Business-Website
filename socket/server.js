9f8b21cae904a91c572bc2b4881e8aba498d2919
James Nguyen <jamesnguyennm@gmail.com>
2023-09-15 22:13:12 +1000

    update port 3000 to 80

--- socket/server.js
+++ socket/server.js
@@ -3,7 +3,7 @@ const server = http.createServer((req, res) => {});
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: ["http://13.211.229.18:3000"],
+    origin: ["http://13.211.229.18"],
     methods: ["GET", "POST"],
   },
 });
