3af92d9ef232f7686331ffca5d9d41165e8feafb
James Nguyen <jamesnguyennm@gmail.com>
2023-09-16 13:36:43 +1000

    update to test ECR & ECS

--- api/app.js
+++ api/app.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
-const app = express();
 const cors = require("cors");
-
-app.use(bodyParser.json());
-app.use(cors());
-
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 
-app.use("/auth", authRoutes);
-app.use("/user", userRoutes);
-app.use("/order", orderRoutes);
-app.use("/admin", adminRoutes);
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/order", orderRoutes);
+app.use("/api/admin", adminRoutes);
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
