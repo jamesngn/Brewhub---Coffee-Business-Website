53ea3ca0ab30b093bbee1298f752af031ca72731
James Nguyen <jamesnguyennm@gmail.com>
2023-09-16 14:43:25 +1000

    test ci

--- api/app.js
+++ api/app.js
@@ -1,28 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const app = express();
 const cors = require("cors");
+
+app.use(bodyParser.json());
+app.use(cors());
+
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 
-const app = express();
-
-// Middleware
-app.use(bodyParser.json());
-app.use(cors());
-
-// Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/order", orderRoutes);
-app.use("/api/admin", adminRoutes);
-
-// Error Handling Middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something went wrong!");
-});
+app.use("/auth", authRoutes);
+app.use("/user", userRoutes);
+app.use("/order", orderRoutes);
+app.use("/admin", adminRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
