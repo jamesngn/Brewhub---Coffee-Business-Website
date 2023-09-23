5ef120a93d972577f6c801ac45eefb91c14bad02
James Nguyen <jamesnguyennm@gmail.com>
2023-09-23 10:47:06 +1000

    add Logout component

---
+++ client/src/components/Logout.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Logout = () => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("authToken");
+  };
+
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+
+  return (
+    <div>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+};
+
+export default Logout;
