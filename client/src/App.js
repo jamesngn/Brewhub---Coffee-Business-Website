7d9431ed86ea46705b4747c857b9abad99cdb8a4
James Nguyen <jamesnguyennm@gmail.com>
2023-10-21 12:23:10 +1100

    UPDATE react routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Dashboard from "./containers/Dashboard/Dashboard";
-import Home from "./containers/Home/Home";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import OrderManagment from "./pages/OrderManagement/OrderManagement";
+import Home from "./pages/Home/Home";
+import Menu from "./pages/Menu/Menu";
+import Order from "./pages/Order/Order";
+import MenuManagment from "./pages/MenuManagement/MenuManagement";
+import AccountManagment from "./pages/AccountManagement/AccountManagement";
+import CategoryManagment from "./pages/CategoryManagement/CategoryManagement";
 
 function App() {
   return (
@@ -12,12 +18,36 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route
-          path="/"
-          element={<ProtectedRoute component={Home} userRequired />}
+          path="/menu"
+          element={<ProtectedRoute component={Menu} userRequired />}
         />
         <Route
-          path="/admin"
+          path="/order-now"
+          element={<ProtectedRoute component={Order} userRequired />}
+        />
+        <Route
+          path="/admin/dashboard"
           element={<ProtectedRoute component={Dashboard} adminRequired />}
+        />
+        <Route
+          path="/admin/order"
+          element={<ProtectedRoute component={OrderManagment} adminRequired />}
+        />
+        <Route
+          path="/admin/menu"
+          element={<ProtectedRoute component={MenuManagment} adminRequired />}
+        />
+        <Route
+          path="/admin/category"
+          element={
+            <ProtectedRoute component={CategoryManagment} adminRequired />
+          }
+        />
+        <Route
+          path="/admin/account"
+          element={
+            <ProtectedRoute component={AccountManagment} adminRequired />
+          }
         />
       </Routes>
     </Router>
