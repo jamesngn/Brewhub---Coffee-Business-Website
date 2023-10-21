f69e5239b0930da691b64ffa2d91e6fdbc783f85
James Nguyen <jamesnguyennm@gmail.com>
2023-10-21 12:22:30 +1100

    update User API

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -22,4 +22,68 @@ router.post("/register", (req, res) => {
   });
 });
 
+router.post("/add-cart", (req, res) => {
+  const { userId, itemId, itemName, price, quantity } = req.body;
+
+  userClient.updateCart(
+    { userId, itemId, itemName, price, quantity },
+    (error, response) => {
+      if (error) {
+        return res.status(500).json({ error: error });
+      } else {
+        if (response.success) {
+          return res
+            .status(200)
+            .json({ success: true, message: response.message });
+        } else {
+          return res
+            .status(200)
+            .json({ success: false, message: response.message });
+        }
+      }
+    }
+  );
+});
+
+router.get("/get-cart-items", (req, res) => {
+  const userId = req.query.userId;
+
+  userClient.getCartItems({ userId }, (error, response) => {
+    if (error) {
+      return res.status(500).json({ error: error });
+    } else {
+      return res.status(200).json(response.items);
+    }
+  });
+});
+
+router.get("/retrieve-name", (req, res) => {
+  const userId = req.query.userId;
+  userClient.getUserName({ userId }, (error, response) => {
+    if (error) {
+      return res.status(500).json({ error: error });
+    } else {
+      if (response) {
+        return res.status(200).json(response);
+      } else {
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+    }
+  });
+});
+
+router.get("/retrieve-all-users", (req, res) => {
+  userClient.getAllUsers({}, (error, response) => {
+    if (error) {
+      return res.status(500).json({ error: error });
+    } else {
+      if (response) {
+        return res.status(200).json(response);
+      } else {
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+    }
+  });
+});
+
 module.exports = router;
