dc506fa53160f76ca8d7a72ac32c15f2cff63bf7
James Nguyen <jamesnguyennm@gmail.com>
2023-10-21 12:21:55 +1100

    update Menu API

--- api/routes/menuRoutes.js
+++ api/routes/menuRoutes.js
@@ -17,8 +17,40 @@ router.post("/add", (req, res) => {
   );
 });
 
+router.get("/getItemByCategoryId", (req, res) => {
+  const categoryId = req.query.categoryId;
+  menuClient.getMenuItemsByCategoryId({ categoryId }, (error, response) => {
+    if (error) {
+      return res.status(500).json({ error: error });
+    } else {
+      return res.status(200).json(response);
+    }
+  });
+});
+
 router.get("/getAll", (req, res) => {
   menuClient.getMenuItems({}, (error, response) => {
+    if (error) {
+      return res.status(500).json({ error: error });
+    } else {
+      return res.status(200).json(response);
+    }
+  });
+});
+
+router.get("/getAllWithCategoryInfo", (req, res) => {
+  menuClient.getMenuItemsWithCategoryInfo({}, (error, response) => {
+    if (error) {
+      return res.status(500).json({ error: error });
+    } else {
+      return res.status(200).json(response);
+    }
+  });
+});
+
+router.delete("/delete", (req, res) => {
+  const itemId = req.query.itemId;
+  menuClient.deleteMenuItem({ id: itemId }, (error, response) => {
     if (error) {
       return res.status(500).json({ error: error });
     } else {
