665994288983bdfa9e9f73c1c327d2a7b318bfa0
James Nguyen <jamesnguyennm@gmail.com>
2023-10-21 12:30:51 +1100

    add OrderSummary table

---
+++ client/src/components/OrderSummary/OrderSummary.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Box, Typography, Paper } from "@mui/material";
+const OrderSummary = ({ subtotal, discounts, tax }) => {
+  const customFontStyle = {
+    fontFamily: "Old Standard TT, serif", // Apply the font
+  };
+  const orderSumItemStyle = {
+    display: "flex",
+    justifyContent: "space-between",
+    marginBottom: "10px",
+  };
+  return (
+    <Box padding={"23px 40px 0"}>
+      <Typography variant="h5" style={customFontStyle} color={"#3E2723"}>
+        Order Summary
+      </Typography>
+      <Paper sx={{ marginTop: "10px", padding: "15px" }}>
+        <Box sx={orderSumItemStyle}>
+          <Typography variant="body2" fontWeight={"bold"}>
+            Subtotal
+          </Typography>
+          <Typography variant="body2" fontWeight={"bold"}>
+            ${subtotal}
+          </Typography>
+        </Box>
+        <Box sx={orderSumItemStyle}>
+          <Typography variant="body2" fontWeight={"bold"} color={"#CD5C5C"}>
+            Discounts
+          </Typography>
+          <Typography variant="body2" fontWeight={"bold"} color={"#CD5C5C"}>
+            -$2.00
+          </Typography>
+        </Box>
+        <Box sx={orderSumItemStyle}>
+          <Typography variant="body2" fontWeight={"bold"}>
+            Sales Tax
+          </Typography>
+          <Typography variant="body2" fontWeight={"bold"}>
+            $1.00
+          </Typography>
+        </Box>
+        <div
+          style={{
+            height: "2px",
+            backgroundColor: "#3E2723",
+            width: "100%",
+            marginTop: "15px",
+            marginBottom: "15px",
+          }}
+        ></div>
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Typography variant="h5" fontWeight={"bold"}>
+            TOTAL
+          </Typography>
+          <Typography variant="h5" fontWeight={"bold"}>
+            ${subtotal + discounts + tax}
+          </Typography>
+        </Box>
+      </Paper>
+    </Box>
+  );
+};
+
+export default OrderSummary;
