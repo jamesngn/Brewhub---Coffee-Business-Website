253f02f1c36f8eddc5d99d8671607aec73bf7da1
James Nguyen <jamesnguyennm@gmail.com>
2023-10-28 22:52:45 +1100

    update Dashboard component

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -4,29 +4,13 @@ import { Box, Typography } from "@mui/material";
 import Sidebar from "../../components/Sidebar/Sidebar";
 
 import useSocket from "../../hooks/useSocket";
-import AddMenuItemForm from "../../components/AddMenuItemForm";
-import AddCategoryForm from "../../components/AddCategoryForm/AddCategoryForm";
-import Logout from "../../components/Logout";
-
-import { getOrderDetails } from "../../services/orderService";
+import ChartComponent from "../../components/Chart/MyChart";
+import MainDash from "../../components/MainDash/MainDash";
+import RightSide from "../../components/RigtSide/RightSide";
 
 const Dashboard = ({ userId, userRole }) => {
   const { socket, response } = useSocket(userId, userRole);
   const [newOrderList, setNewOrderList] = useState([]);
-  useEffect(() => {
-    //Retrieve order info by orderId
-    async function FetchOrderById() {
-      try {
-        const orderId = JSON.parse(response).orderId;
-        const orderDetails = await getOrderDetails(orderId);
-        setNewOrderList((prevOrderList) => [...prevOrderList, orderDetails]);
-        console.log(orderDetails);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    FetchOrderById();
-  }, [response]);
 
   const handleSend = (data) => {
     // Send a message using the existing socket connection
@@ -56,49 +40,9 @@ const Dashboard = ({ userId, userRole }) => {
   }
 
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box sx={{ display: "flex", backgroundColor: "#F5DEB3" }}>
       <Sidebar />
-
-      <Box>
-        <Typography variant="h4">Dashboard</Typography>
-        <Typography variant="body1">
-          Welcome to the Admin Dashboard !
-        </Typography>
-        <div>
-          <strong>Server Message:</strong> {response}
-        </div>
-        <div>
-          <h1>Order List</h1>
-          <ul>
-            {newOrderList.map((order, index) => (
-              <li key={index}>
-                <strong>Order ID:</strong> {order.orderId}
-                <br />
-                <strong>Customer Name:</strong> {order.userId}
-                <br />
-                <strong>Date & Time:</strong> {formatDate(order.orderDate)}
-                <br />
-                <strong>Order Status:</strong> {order.orderStatus}
-                <br />
-                <strong>Total Price:</strong> ${order.totalAmount}
-                <br />
-                <strong>Items:</strong>
-                <ul>
-                  {order.orderItems.map((item, itemIndex) => (
-                    <li key={itemIndex}>
-                      {item.itemName} - Price: ${item.price}, Quantity:{" "}
-                      {item.quantity}, Subtotal: ${item.subtotal}
-                    </li>
-                  ))}
-                </ul>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <AddMenuItemForm />
-        <AddCategoryForm />
-        <Logout />
-      </Box>
+      <MainDash />
     </Box>
   );
 };
