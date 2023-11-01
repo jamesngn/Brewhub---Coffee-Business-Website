c489050a8c24b732f409fbbdbd5e5a415340353e
James Nguyen <jamesnguyennm@gmail.com>
2023-11-01 17:47:21 +1100

    update cartsummary

--- client/src/components/CartSummary/CartSummary.js
+++ client/src/components/CartSummary/CartSummary.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, IconButton } from "@mui/material";
+import ClearIcon from "@mui/icons-material/Clear";
 
 const CartItem = ({ name, quantity, price }) => {
   const cartSumItemStyle = {
@@ -30,45 +31,61 @@ const CartItem = ({ name, quantity, price }) => {
           fontWeight={"bold"}
           color={"rgba(62,39,35,0.51)"}
         >
-          ${price * quantity}
+          ${(price * quantity).toFixed(2)}
         </Typography>
       </Box>
-      <Box flex={0.1}></Box>
     </Box>
   );
 };
 
-const CartSummary = ({ cartItems }) => {
+const CartSummary = ({ cartItems, onClearCart }) => {
   const customFontStyle = {
     fontFamily: "Old Standard TT, serif", // Apply the font
   };
 
   return (
     <Box padding={"23px 40px 0"}>
-      <Typography
-        sx={{ display: "inline-block" }}
-        variant="h5"
-        style={customFontStyle}
-        color={"#3E2723"}
-      >
-        Cart Summary
-      </Typography>
-      <Typography
-        sx={{
-          backgroundColor: "#3E2723",
-          color: "#FFFFF0",
-          borderRadius: "50%",
-          textAlign: "center",
-          display: "inline-block",
-          width: "30px",
-          height: "30px",
-          marginLeft: "10px",
-        }}
-        variant="h6"
-        style={customFontStyle}
-      >
-        {cartItems.length}
-      </Typography>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div>
+          <Typography
+            sx={{ display: "inline-block" }}
+            variant="h5"
+            style={customFontStyle}
+            color={"#3E2723"}
+          >
+            Cart Summary
+          </Typography>
+          <Typography
+            sx={{
+              backgroundColor: "#3E2723",
+              color: "#FFFFF0",
+              borderRadius: "50%",
+              textAlign: "center",
+              display: "inline-block",
+              width: "30px",
+              height: "30px",
+              marginLeft: "10px",
+            }}
+            variant="h6"
+            style={customFontStyle}
+          >
+            {cartItems.length}
+          </Typography>
+        </div>
+        {/* Clear Cart Button */}
+        <IconButton
+          onClick={onClearCart}
+          sx={{
+            backgroundColor: "#3E2723",
+            color: "#FFFFF0",
+            marginLeft: "10px",
+            width: "30px",
+            height: "30px",
+          }}
+        >
+          <ClearIcon />
+        </IconButton>
+      </div>
       <Box
         height={"45vh"}
         overflow={"auto"}
