1926f88a1f120ecce047cad1e4e4f1d2729286d5
James Nguyen <jamesnguyennm@gmail.com>
2023-11-01 17:50:11 +1100

    add func to getOrderhistory and updateOrderStatus

--- client/src/services/orderService.js
+++ client/src/services/orderService.js
@@ -36,4 +36,34 @@ const getAllOrderDetails = async () => {
   }
 };
 
-export { placeOrder, getOrderDetails, getAllOrderDetails };
+const getOrderHistory = async (userId) => {
+  try {
+    const response = await axios.get(
+      `http://${config.publicIpAddress}:5000/order/retrieveOrderHistory`,
+      { params: { userId: userId } }
+    );
+    return response.data.orders;
+  } catch (error) {
+    console.error("Error retrieving order history:", error);
+  }
+};
+
+const updateOrderStatus = async (orderId, newStatus) => {
+  try {
+    const response = await axios.put(
+      `http://${config.publicIpAddress}:5000/order/updateOrderStatus`,
+      { orderId, newStatus }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error updating order status:", error);
+  }
+};
+
+export {
+  placeOrder,
+  getOrderDetails,
+  getAllOrderDetails,
+  getOrderHistory,
+  updateOrderStatus,
+};
